86d8d4d5f44b9f2535839897618bd69d1e77174f
ShoopDeWoop <116699852+Rikuron@users.noreply.github.com>
2025-11-27 20:06:23 +0800

    Fix CORS 2

--- app.py
+++ app.py
@@ -17,13 +17,10 @@ app = Flask(__name__)
 CORS(
     app, 
     origins=[
-        "http://localhost:5173",
-        "http://localhost:3000",
+        r"http://localhost:\d+",
         "https://itd-112-case-study.vercel.app",
-        "https://*.vercel.app"
+        r"https://.*\.vercel\.app"
     ],
-    methods=['GET', 'POST', 'OPTIONS'],
-    allow_headers=['Content-Type', 'Authorization'],
     supports_credentials=True
 )
 
